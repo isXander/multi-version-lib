3ded66a6c846388c3e6a015e76fc1755fb22d9b2
isXander <xandersmith2008@gmail.com>
2022-12-06 20:25:59 +0000

    remove useless version catalog

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -8,11 +8,6 @@ pluginManagement {
 
 rootProject.name = "multi-version-lib"
 
-dependencyResolutionManagement {
-    versionCatalogs {
-        create("libs")
-    }
-}
 include("api")
 include("impl11802")
 include("impl11902")
